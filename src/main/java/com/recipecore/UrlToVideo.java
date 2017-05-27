c207191afc02ca56a64d4d9a1eaeec4bf36735cf
Rizwan Gilani <rizwangilani@Rizwans-MacBook-Pro.local>
2017-05-27 19:57:46 -0400

    initial commit.

---
+++ src/main/java/com/recipecore/UrlToVideo.java
@@ -0,0 +1,26 @@
+package com.recipecore;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by rizwangilani on 5/27/17.
+ */
+public class UrlToVideo {
+
+    public static void downloadUsingStream(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(file);
+        byte[] buffer = new byte[1024];
+        int count=0;
+        while((count = bis.read(buffer,0,1024)) != -1)
+        {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
+    }
+}
---
+++ tst/UrlToVideoTest.java
@@ -0,0 +1,14 @@
+import com.recipecore.UrlToVideo;
+import org.junit.Test;
+
+/**
+ * Created by rizwangilani on 5/27/17.
+ */
+public class UrlToVideoTest {
+
+    @Test
+    public void test() throws Exception {
+//        UrlToVideo.downloadUsingStream("https://www.facebook.com/buzzfeedtasty/videos/1922870511298922/", "testfile.html");
+        System.out.println("This works!");
+    }
+}
